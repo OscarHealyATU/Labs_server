e9d841613cbc841cf42e8c81361129f81662dbf3
Oscar <g00424324@atu.ie>
2025-10-22 14:31:54 +0000

    hello world -> hello Oscar (ish)

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// re short for request, res short for response
+// when someone visits site route server sends "hello ..."
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
 });
-
+// starts server & listens for requests 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
